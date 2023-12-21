8e3a2713fab476f30097a144525e405ef9e10b31
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2023-12-20 21:41:03 -0300

    Troca de paleta de acordo com a hora

--- scripts/script.js
+++ scripts/script.js
@@ -3,23 +3,61 @@ document.addEventListener('DOMContentLoaded', function() {
     var body = document.body;
 
     function atualizarTema() {
-      var horaAtual = 21//new Date().getHours();
-      var modoNoturno = horaAtual >= 18 || horaAtual < 6;
+      var horaAtual = new Date().getHours();
 
-      if (modoNoturno) {
-        root.style.setProperty('--cor0', '#00a6fb')
-        root.style.setProperty('--cor1', '#0582ca');
-        root.style.setProperty('--cor2', '#006494');
-        root.style.setProperty('--cor3', '#003554');
-        root.style.setProperty('--cor4', '#051923');
-        root.style.setProperty('--cor5', '#000000');
-      } else {
-        root.style.setProperty('--cor0', '#fcebeb')
-        root.style.setProperty('--cor1', '#ffa8a8');
-        root.style.setProperty('--cor2', '#ff6262');
-        root.style.setProperty('--cor3', '#ff0000');
-        root.style.setProperty('--cor4', '#8a0000');
-        root.style.setProperty('--cor5', '#330000');
+      switch(horaAtual) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            root.style.setProperty('--cor0', '#dadada');
+            root.style.setProperty('--cor1', '#7e7e7e');
+            root.style.setProperty('--cor2', '#494949');
+            root.style.setProperty('--cor3', '#302f2f');
+            root.style.setProperty('--cor4', '#202020');
+            root.style.setProperty('--cor5', '#000000');
+            break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+            root.style.setProperty('--cor0', '#000000');
+            root.style.setProperty('--cor1', '#e69e0a');
+            root.style.setProperty('--cor2', '#d18c07');
+            root.style.setProperty('--cor3', '#bc7b03');
+            root.style.setProperty('--cor4', '#a76900');
+            root.style.setProperty('--cor5', '#633f00');
+            break;
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+            root.style.setProperty('--cor0', '#fcebeb');
+            root.style.setProperty('--cor1', '#ffa8a8');
+            root.style.setProperty('--cor2', '#ff6262');
+            root.style.setProperty('--cor3', '#ff0000');
+            root.style.setProperty('--cor4', '#8a0000');
+            root.style.setProperty('--cor5', '#330000');
+            break;
+        case 18:
+        case 19:
+        case 20:
+        case 21:
+        case 22:
+        case 23:
+            root.style.setProperty('--cor0', '#eeeeee');
+            root.style.setProperty('--cor1', '#407dff');
+            root.style.setProperty('--cor2', '#0051ff');
+            root.style.setProperty('--cor3', '#003ab6');
+            root.style.setProperty('--cor4', '#002679');
+            root.style.setProperty('--cor5', '#000e2c');
+            break;
       }
     }
 
